d4a4b91d7ab26b91143fc3df778c7bb991eb1934
Jaredmutai <jaredmutai01j@gmail.com>
2025-04-02 23:26:45 +0300

    App-module

--- simple-task-list/src/app/app.module.ts
+++ simple-task-list/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // Required for ngModel
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HighlightDirective],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
