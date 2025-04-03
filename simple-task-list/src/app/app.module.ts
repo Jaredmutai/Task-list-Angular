9db7dfc725bfbd9ca56330a528527430bc0643c2
Jaredmutai <jaredmutai01j@gmail.com>
2025-04-03 21:02:18 +0300

    added CapitalizePipe to declarations

--- simple-task-list/src/app/app.module.ts
+++ simple-task-list/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
+import { CapitalizePipe } from './capitalize.pipe';
 
 @NgModule({
-  declarations: [AppComponent, HighlightDirective],
+  declarations: [AppComponent, HighlightDirective, CapitalizePipe],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent]
 })
