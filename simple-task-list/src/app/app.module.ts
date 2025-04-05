ff16294a60bf2f404b123f28549639a9da3b53a8
Jaredmutai <jaredmutai01j@gmail.com>
2025-04-06 01:42:30 +0300

    app module

--- simple-task-list/src/app/app.module.ts
+++ simple-task-list/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HighlightDirective } from './highlight.directive';
-import { CapitalizePipe } from './capitalize.pipe';
+import { CapitalizePipe } from './capitalize.pipes';
+import { TaskService } from './task.service';
 
 @NgModule({
   declarations: [AppComponent, HighlightDirective, CapitalizePipe],
   imports: [BrowserModule, FormsModule],
+  providers: [TaskService], // Optional: only needed if not using providedIn: 'root'
   bootstrap: [AppComponent]
 })
 export class AppModule {}
