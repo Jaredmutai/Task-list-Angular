6d02010d80ebc703e5864a4618ac27fe6a3dbdc1
Jaremutai <jaremutai01j@gmail.com>
2025-04-18 23:44:10 +0300

    services-task-service

--- task-creation-app/src/app/services/task.service.ts
+++ task-creation-app/src/app/services/task.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
 
+export interface Task {
+  title: string;
+  description: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private tasks: Task[] = [];
 
-  constructor() { }
+  getTasks(): Task[] {
+    return this.tasks;
+  }
+
+  addTask(task: Task): void {
+    this.tasks.push(task);
+  }
 }
