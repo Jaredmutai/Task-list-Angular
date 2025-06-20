51994a81f12113da0e96a1f73b89e64aa05b6118
Jaredmutai <jaredmutai01j@gmail.com>
2025-06-20 23:00:53 +0300

    reactive form component

--- task-creation-app/src/app/reactive-form/reactive-form.component.spec.ts
+++ task-creation-app/src/app/reactive-form/reactive-form.component.spec.ts
@@ -20,4 +20,46 @@ describe('ReactiveFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a form with initial values', () => {
+    expect(component.form.value).toEqual({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    });
+  });
+  it('should validate the form correctly', () => {
+    const form = component.form;
+    expect(form.valid).toBeFalse();
+
+    form.controls['firstName'].setValue('John');
+    form.controls['lastName'].setValue('Doe');
+    form.controls['email'].setValue('johdoe@hmail.com');
+    form.controls['password'].setValue('password123');  
+    expect(form.valid).toBeTrue();
+    expect(form.value).toEqual({
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'johndoe@hmail.com',
+      password: 'password123' 
+    });
+  }
+  );
+  it('should reset the form', () => {
+    const form = component.form;
+    form.controls['firstName'].setValue('John');
+    form.controls['lastName'].setValue('Doe');
+    form.controls['email'].setValue('johndoe@gmail.com');
+    form.controls['password'].setValue('password123');  
+    expect(form.valid).toBeTrue();
+    component.resetForm();
+    expect(form.value).toEqual({
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: ''
+    });
+
+  
 });
